beaa75f9c34dd3bb8b6f9aa32d9d9201cdfb5b0d
Tapawingo <tapawingogamer@gmail.com>
2024-09-01 17:54:05 +0200

    Fixed middleware CSRF security

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,31 +1,32 @@
 import { Lucia } from 'lucia';
 import { D1Adapter } from '@lucia-auth/adapter-sqlite';
 
+import type { D1Database } from '@nuxthub/core';
 import type { DatabaseUser } from './userDB';
 
-const db = hubDatabase();
-
-const adapter = new D1Adapter(db, {
-  user: "user",
-  session: "session"
-});
-
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: !import.meta.dev /* Determines if the session is httpOnly */
+export function initializeLucia(D1: D1Database) {
+  const adapter = new D1Adapter(D1, {
+    user: "user",
+    session: "session"
+  });
+  
+  return new Lucia(adapter, {
+    sessionCookie: {
+      attributes: {
+        secure: !import.meta.dev /* Determines if the session is httpOnly */
+      }
+    },
+    getUserAttributes: (attributes) => {
+      return {
+        username: attributes.username
+      }
     }
-  },
-  getUserAttributes: (attributes) => {
-    return {
-      username: attributes.username
-    }
-  }
-});
+  });
+}
 
 declare module "lucia" {
   interface Register {
-    Lucia: typeof lucia;
+    Lucia: ReturnType<typeof initializeLucia>;
     DatabaseUserAttributes: Omit<DatabaseUser, "id">
   }
 }
